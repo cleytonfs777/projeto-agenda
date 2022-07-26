1e868905ea5383d6a9206fa20bca5c3c733843be
cleytonfs777 <cleyton.fs777@gmail.com>
2022-07-26 20:59:40 -0300

    projeto finalizado

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.urls import is_valid_path
+from .models import FormContato
 
 def login(request):
     if request.method != 'POST':
@@ -78,4 +80,25 @@ def cadastro(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != 'POST':
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form = FormContato(request.POST, request.FILES)
+
+    if not form.is_valid():
+        messages.error(request, 'Erro ao enviar formulário.')
+        form = FormContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    descricao = request.POST.get('descricao')
+    print(descricao)
+
+    if len(descricao) < 5:
+        messages.error(request, 'Descrição precisa ter mais de 5 caracteres.')
+        form = FormContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form.save()
+    messages.success(request, f'Dados do {request.POST.get("nome")} enviados com sucesso!')
+    return redirect('dashboard')
--- contatos/models.py
+++ contatos/models.py
@@ -13,7 +13,7 @@ class Contato(models.Model):
     telefone = models.CharField(max_length=255)
     email = models.CharField(max_length=255, blank=True)
     data_criacao = models.DateTimeField(default=timezone.now)
-    descriao = models.TextField(blank=True)
+    descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
     mostrar = models.BooleanField(default=True)
     foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m/')
